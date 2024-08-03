1f6dc61761872fcb0d17b128de935913f7b98e36
allan <4lwachira@gmail.com>
2024-08-03 11:43:12 +0300

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+function Blur() {
+    console.log("Hey! Eyes on me!");
+    
+}
+function Focus() {
+  console.log("Good!");
+    
+}
+
+const EyesOnMe = () => {
+  return (
+    <div>
+    <button onFocus={Focus} onBlur={Blur}> Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+    function change() {
+        console.log("Entering password");
+        
+    }
+  return (
+    <div>
+      <input type="password" onChange={change} />
+    </div>
+  );
 }
 
 export default Keypad;
